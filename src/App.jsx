7841dd85fbbaa079bdb9992923deebc42d2a64ae
iqmalhabib <crasengan44@gmail.com>
2024-03-23 23:29:56 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import styles from "./App.module.css";
-import {Hero} from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { About} from "./components/About/About";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
-  <div className={styles.App}>
-    <Navbar/>
-    <Hero/>
-    <About/>
-  </div>
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
